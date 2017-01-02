5471bc68b54e7ea2bfa8cb7654e8da62575d24c4
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-03 00:46:19 +0900

    Fixed about installing command line tool

--- setup/install_command_line_tool.sh
+++ setup/install_command_line_tool.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-xcodebuild -version >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-  sudo xcodebuild -license
-  sudo xcode-select --install
-fi
+sudo xcodebuild -license
+sudo xcode-select --install
