9bc47eea4418678d57992638800b5891a9dec920
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-03 20:02:17 +0900

    Python Uesr site package

--- setup/install_ansible.sh
+++ setup/install_ansible.sh
@@ -4,8 +4,9 @@ which pip >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
   echo "pip not found"
   echo "installing pip..."
+  mkdir -p ~/Library/Python/2.7/lib/python/site-packages
   easy_install --user pip
 fi
 
 echo "installing ansible..."
-pip install -U --user -r $(dirname $0)/requirements.txt
+pip install -U -r $(dirname $0)/requirements.txt
