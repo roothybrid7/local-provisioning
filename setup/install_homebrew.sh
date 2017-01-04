5ad826fbd3d1a6254ec2a3b990483c4043f3f18e
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-04 11:52:04 +0900

    Added install script for homebrew

---
+++ setup/install_homebrew.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if which brew >/dev/null 2>&1; then
+  echo "Homebrew is already installed"
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
