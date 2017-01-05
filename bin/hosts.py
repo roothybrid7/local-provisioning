44033072598b876a3a31a0ed08ac3f02aad1f32b
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-05 11:30:03 +0900

    Added a host var about root path

--- bin/hosts.py
+++ bin/hosts.py
@@ -8,6 +8,8 @@ import sys
 def main():
     python_interpreter = os.popen('which python').read().rstrip()
     inventory = "localhost_%s" % sys.platform
+    dirpath = os.path.abspath(os.path.dirname(__file__))
+    rootpath = os.path.dirname(dirpath)
 
     print json.dumps({
         inventory: {
@@ -19,6 +21,7 @@ def main():
             'hostvars': {
                 '127.0.0.1': {
                     'ansible_python_interpreter': python_interpreter,
+                    'ansible_root_path': rootpath,
                 }
             }
         }
