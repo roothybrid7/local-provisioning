82be011328e0854afec341593cd26f28c0842fdf
Satoshi Ohki <roothybrid7@gmail.com>
2018-05-14 19:36:06 +0900

    Fixed for the issue of gathering facts

--- setup/play_ansible.sh
+++ setup/play_ansible.sh
@@ -5,7 +5,7 @@ if [[ ! -e "roles/williamyeh.oracle-java" ]]; then
 fi
 
 if [[ -f ".private.yml" ]]; then
-  ansible-playbook site.yml --extra-vars="@.private.yml" --ask-vault-pass -vv
+  ansible-playbook site_darwin.yml --extra-vars="@.private.yml" --ask-vault-pass -vv
 else
   echo "\033[101m.private.yml not found!!!\033[0m"
   exit 1
